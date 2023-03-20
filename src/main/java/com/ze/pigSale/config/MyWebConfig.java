81355b5635222a6d1fb89611b6136391f76906d2
zeb <2829806354@qq.com>
2023-03-20 16:50:50 +0800

    使用swagger开发工具

--- src/main/java/com/ze/pigSale/config/MyWebConfig.java
+++ src/main/java/com/ze/pigSale/config/MyWebConfig.java
@@ -1,13 +1,24 @@
 package com.ze.pigSale.config;
 
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import com.ze.pigSale.common.JacksonObjectMapper;
 import com.ze.pigSale.interceptor.LoginInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
@@ -15,7 +26,10 @@ import java.util.List;
  * author: zebii
  * Date: 2023-01-28-20:35
  */
+@EnableSwagger2
+@EnableKnife4j
 @Configuration
+@Slf4j
 public class MyWebConfig implements WebMvcConfigurer {
 
 //    @Override
@@ -36,11 +50,51 @@ public class MyWebConfig implements WebMvcConfigurer {
 //                        "/user/sendMsg",
 //                        "/");
 //    }
+//              "/employee/login",
+//            "/employee/login",
+//            "/backend/**",
+//            "/front/**",
+//            "/user/sendMsg",
+//            "/user/login",
+//            "/doc.html",
+//            "/webjars/**",
+//            "/swagger-resources",
+//            "/v2/api-docs"
+
+    /**
+     * 设置静态资源映射
+     * */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //添加映射
+        log.info("映射资源开始");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
         messageConverter.setObjectMapper(new JacksonObjectMapper());
         converters.add(0, messageConverter);
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        // 文档类型
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ze.pigSale.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("猪联网电子商务")
+                .version("1.0")
+                .description("pigSale接口文档")
+                .build();
     }
 }
